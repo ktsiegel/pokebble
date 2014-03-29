e7134aab8a5c1a77defda58be29f8e5e7a15c94a
kathrynsiegel <ksiegel@mit.edu>
2014-03-29 18:55:42 -0400

    web interface

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, request, session, render_template, jsonify, redirect, url_for
-from flask import render_template
-from flask import url_for
+
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 def game():
-	return "hello world"
+	return render_template('game.html')
+
+@app.route("/configure")
+def configure():
+	return render_template('configure.html')
 
 if __name__ == "__main__":
 	app.debug = True
